141557f342c0b5ff13679b41d3f8c6e1ef17572f
Petr kecinzer Řezníček <kecinzer@gmail.com>
2014-10-10 13:40:10 +0200

    I don't need this anymore
    Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,13 +22,6 @@ TARGET_KERNEL_CONFIG := franco_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
 
-# Optimizations
-OPT_A_LOT := true
-MAKE_STRICT_GLOBAL := true
-OPT_MEMORY := true
-ENABLE_GRAPHITE := true
-OPTIMIZE_LINKER := true
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
 
